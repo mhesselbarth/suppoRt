2ab6269fd30ce033d2db96f16ddb4ad8fe1d61ba
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-02-27 11:10:46 +0100

    Bugfixes
    Bugfixes to Save.Function.ggplot and minor changes to printing

--- R/Save_Function_ggplot.R
+++ R/Save_Function_ggplot.R
@@ -6,18 +6,23 @@
 #' @param filename [\code{string(1)}]\cr File name to create on disk
 
 #' @export
-Save.Function.ggplot <- function(plot, filename, ...){
-  if (file.exists(filename)){
-    input <- utils::menu(title="Do you want to overwrite already existing file?", choices=c("Yes", "No"))
+Save.Function.ggplot <- function(plot, filename, path, ...){
+
+  complete_file <- base::file.path(path, filename)
+
+  base::print(base::paste0("Trying to save plot: ", complete_file))
+
+  if (base::file.exists(complete_file)){
+    input <- utils::menu(title="Do you want to overwrite already existing ggplot?", choices=c("Yes", "No"))
     if(input==1){
-      ggplot2::ggsave(plot=plot, filename=filename, ...)
-      base::print("Existing file overwriten")
+      ggplot2::ggsave(plot=plot, filename=filename, path=path, ...)
+      base::print("Existing ggplot overwriten")
     }
-    else{base::print("File not saved")}
+    else{base::print("ggplot not saved")}
 
   }
   else{
-    ggplot2::ggsave(plot=plot, filename=filename, ...)
-    base::print("New file written")
+    ggplot2::ggsave(plot=plot, filename=filename, path=path, ...)
+    base::print("New ggplot written")
   }
 }
