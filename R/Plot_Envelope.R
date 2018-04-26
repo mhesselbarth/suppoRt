0104374317786b2d5452caa30fcba63ece9ae24c
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-04-26 14:31:44 +0200

    Improvments to Plot.Envelope and overwrite = FALSE for saving

--- R/Plot_Envelope.R
+++ R/Plot_Envelope.R
@@ -10,57 +10,76 @@
 #' \cr 1 = obs > hi
 #' \cr 2 = lo < obs < hi
 #' \cr 3 = obs < lo
+#' @param title [\code{string(1)}]\cr Title of the plot
+#' @param xlab [\code{string(1)}]\cr x label of the plot
+#' @param ylab [\code{string(1)}]\cr y label of the plot
 #' @param full_fun [\code{logical(1)}]\cr Plot full function or only line to indicate
+#' @param standarized [\code{logical(1)}]\cr If TRUE obs = obs - theo
 #' deviation from null model
 
-#' @importFrom magrittr "%>%"
 #' @export
-Plot.Envelope <- function(input, labels=c('clustering', ' randomness', 'segregation'),
-                          full_fun=T){
+Plot.Envelope <- function(input,
+                          labels = c('clustering', ' randomness', 'segregation'),
+                          title = NULL, xlab = NULL, ylab = NULL,
+                          full_fun = T,
+                          standarized = F){
 
-  if(!is(input, "envelope")){stop('Please provide envelope object of the spatstat package')}
+  if(!is(input, 'envelope')){stop('Please provide envelope object of the spatstat package')}
 
-  if(length(labels) != 3){
+  if(length(labels) !=  3){
     labels <- c('clustering', ' randomness', 'segregation')
     print('Not enough labels provided - using clustering, randomness and segregation')
   }
 
+  if(is.null(xlab)){xlab <- 'r'}
+  if(is.null(ylab)){ylab <- 'f(r)'}
+
   data <- input %>%
     tibble::as.tibble() %>%
-    dplyr::mutate(type=dplyr::case_when(obs > hi ~ labels[1],
-                                        obs > lo & obs < hi ~ labels[2],
-                                        obs < lo ~ labels[3]))
+    setNames(c('r', 'obs', 'theo', 'lo', 'hi'))
+
+  if(standarized == TRUE){
+    data <- data %>%
+      dplyr::mutate(obs = obs - theo,
+                    lo = lo - theo ,
+                    hi = hi - theo,
+                    theo = theo - theo)
+  }
+
+  data <- data %>%
+    dplyr::mutate(type = dplyr::case_when(obs > hi ~ labels[1],
+                                          obs >=  lo & obs <=  hi ~ labels[2],
+                                          obs < lo ~ labels[3]))
+
+  color_scale <- c('#440154FF',
+                   '#238A8DFF',
+                   '#FDE725FF')
+  names(color_scale) <- labels
 
   if(full_fun == T){
 
-    # color_scale <- c(labels[1] = "#440154FF",
-    #                  labels[2] = "#238A8DFF",
-    #                  labels[3] = "#FDE725FF")
-
     gg_plot <- data %>%
       ggplot2::ggplot() +
-      ggplot2::geom_ribbon(ggplot2::aes(x=r, ymin=lo, ymax=hi), fill='grey', alpha=0.3) +
-      ggplot2::geom_line(ggplot2::aes(x=r, y=obs, linetype='Observed')) +
-      ggplot2::geom_line(ggplot2::aes(x=r, y=theo, linetype='Theoretical')) +
-      ggplot2::geom_line(ggplot2::aes(x=r, y=min(c(lo, obs)), colour=type, group=type), size=2.5) +
-      ggplot2::scale_color_manual(name='', values=c("#440154FF", "#238A8DFF", "#FDE725FF")) +
-      # ggplot2::scale_color_manual(name='', values=color_scale) +
-      ggplot2::scale_linetype_manual(name='', values=c(1,2)) +
-      ggplot2::labs(x='r', y='f(r)') +
-      ggplot2::theme_bw()
+      ggplot2::geom_ribbon(ggplot2::aes(x = r, ymin = lo, ymax = hi), fill = 'grey') +
+      ggplot2::geom_line(ggplot2::aes(x = r, y = obs, linetype = 'Observed'), size = 1) +
+      ggplot2::geom_line(ggplot2::aes(x = r, y = theo, linetype = 'Theoretical'), size = 0.75) +
+      ggplot2::geom_line(ggplot2::aes(x = r, y = min(c(lo, obs)), colour = type, group = 'x'), size = 2.5) +
+      ggplot2::scale_color_manual(name = '', values = color_scale) +
+      ggplot2::scale_linetype_manual(name = '', values = c(1,2)) +
+      ggplot2::labs(x = xlab, y = ylab, title = title) +
+      ggplot2::theme_bw(base_size = 15) +
+      ggplot2::theme(legend.position = 'bottom')
   }
 
   else{
     gg_plot <-  data %>%
       ggplot2::ggplot() +
-      ggplot2::geom_line(ggplot2::aes(x=r, y=0, colour=type, group=type), size=5) +
+      ggplot2::geom_line(ggplot2::aes(x = r, y = 0, colour = type, group = 'x'), size = 5) +
       ggplot2::ylim(0, 0.1) +
-      ggplot2::scale_color_manual(name='', values=c("#440154FF", "#238A8DFF", "#FDE725FF")) +
-      # ggplot2::scale_color_manual(name='', values=color_scale) +
-      ggplot2::scale_linetype_manual(name='', values=c(1,2)) +
-      ggplot2::labs(x='r', y='') +
-      ggplot2::theme_classic() +
-      ggplot2::theme(legend.position="bottom",
+      ggplot2::scale_color_manual(name = '', values = color_scale) +
+      ggplot2::labs(x = xlab, y = '', title = title) +
+      ggplot2::theme_classic(base_size = 15) +
+      ggplot2::theme(legend.position = 'bottom',
                      strip.background = ggplot2::element_blank(),
                      axis.line.y = ggplot2::element_blank(),
                      axis.text.y = ggplot2::element_blank(),
