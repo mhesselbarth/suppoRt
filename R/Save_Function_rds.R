58f358631e89f23e84008e099ec659c0cba2b09e
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-06-11 08:27:01 +0200

    Documentation updated

--- R/Save_Function_rds.R
+++ R/Save_Function_rds.R
@@ -3,7 +3,9 @@
 #' This function checks if the file aready exists before saving it.
 #' See ?base::saveRDS() for more information
 #' @param object [\code{R object(1)}]\cr R object to serialize
-#' @param file [\code{string(1)}]\cr Connection or the name of the file where the R object is saved. The file extension must be .rds
+#' @param filename [\code{string(1)}]\cr Name of the file where the R object is saved. The file extension must be .rds
+#' @param path [\code{string(1)}]\cr Path to where the R object is saved
+#' @param overwrite [\code{logical(1)}]\cr Overwrite if the file alreay exists
 
 #' @export
 Save.Function.rds <- function(object,  filename = NULL, path = NULL, overwrite = FALSE, ...){
@@ -17,9 +19,9 @@ Save.Function.rds <- function(object,  filename = NULL, path = NULL, overwrite =
   if (base::file.exists(complete_file)){
     if(overwrite == TRUE){
       saveRDS(object=object, file=complete_file, ...)
-      print("Existing file overwriten \n")
+      cat("Existing file overwriten \n")
     }
-    else{print("File not saved \n")}
+    else{cat("File not saved \n")}
   }
 
   else{
