0a7016a5b16f6492504a51a62eb7c679cc68f78a
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-12-20 08:34:10 +0100

    Added replace_infinite

---
+++ R/replace_infinite.R
@@ -0,0 +1,77 @@
+#' replace_infinite
+#'
+#' @description Convert `NaN` and `Inf` to `NA` or other value
+#'
+#' @param x vector
+#' @param what What infinite values should be replaced. See details.
+#' @param value Value that will be used to replace selected infinite values.
+#'
+#' @details
+#' The function converts all `NaN` and `Inf` values to NA. Input must be a vector.
+#' What can be either `what = c("NaN", "Inf")` to replace both `NaN` and `Inf` or just
+#' one of the two, e.g. `what = "NaN`.
+#'
+#' @return  vector
+#'
+#' @examples
+#' vec <- c(1, 2, 3, NA, 5, NaN, Inf)
+#' replace_infinite(vec)
+#' replace_infinite(vec, what = "NaN", value = NA)
+#' replace_infinite(vec, value = c(NA, 0))
+#'
+#' @aliases replace_infinite
+#' @rdname replace_infinite
+
+#' @export
+replace_infinite <- function(x, what = c("NaN", "Inf"), value = c(NA, NA)) {
+
+  # check if input is vector
+  if (!is.vector(x)) {
+    stop("input must be a vector.", call. = FALSE)
+  }
+
+  # check if input is numeric
+  if (class(x) != "numeric") {
+    stop("input must be of class 'numeric'.", call. = FALSE)
+  }
+
+  # replace NaN and Inf
+  if(all(what == c("NaN", "Inf"))) {
+
+    x <- dplyr::if_else(is.nan(x), as.numeric(value[[1]]), x)
+
+    x <- dplyr::if_else(is.infinite(x), as.numeric(value[[2]]), x)
+
+    return(x)
+  }
+
+  # only replay NaN
+  else if (what == "NaN") {
+
+    if(length(value) > 1) {
+      warning("Only using first 'value' as replace value", call. = FALSE)
+    }
+
+    x <- dplyr::if_else(is.nan(x), as.numeric(value[[1]]), x)
+
+    return(x)
+  }
+
+  # only replace Inf
+  else if (what == "Inf") {
+
+    if(length(value) > 1) {
+      warning("Only using first 'value' as replace value", call. = FALSE)
+    }
+
+    x <-dplyr::if_else(is.infinite(x), as.numeric(value[[1]]), x)
+
+    return(x)
+  }
+
+  # return error
+  else {
+    stop("'what' must be 'NaN', 'Inf' or 'c(NaN, Inf)'", call. = FALSE)
+  }
+}
+
