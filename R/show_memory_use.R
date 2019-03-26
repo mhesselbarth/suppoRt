464c770c270967a3e36b2a44952c31b2a9fe298b
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-03-26 14:45:27 +0100

    improvments show_memory_use

--- R/show_memory_use.R
+++ R/show_memory_use.R
@@ -5,6 +5,7 @@
 #' @param sort Sort the resulting dataframe either by "size", "alphabetical" or "class".
 #' @param units Unit used to format memory usage. For more details, see \code{?object.size}.
 #' @param decreasing Logical if results should be sorted in decreasing order.
+#' @param n Number of top rows to print.
 #'
 #' @details
 #' Functions returns the name, class and size of all objects in the environment. The
@@ -24,28 +25,37 @@
 #' @rdname show_memory_use
 
 #' @export
-show_memory_use <- function(sort = "size", units = "Kb", decreasing = FALSE) {
+show_memory_use <- function(sort = "size", units = "Kb", decreasing = TRUE, n = NULL) {
 
+  # get all present objects in environment
   present_objects <- ls(parent.frame())
 
+  # stop if environment is empty
   if (length(present_objects) == 0) {
     stop("No objects in environment.", call. = FALSE)
   }
 
+  # get memory usage and class
   memory_usage <- lapply(present_objects, function(x) {
 
-    current_size <- format(utils::object.size(x), units = units)
+    # get current size and split into numeric and unit
+    current_size <- format(utils::object.size(get(x)), units = units)
     current_size <- strsplit(current_size, split = " ")[[1]]
 
-    current_class <- class(get(x))
+    # get class of object
+    current_class <- class(get(x))[[1]]
 
-    data.frame(class = as.character(current_class),
+    # combine to one df
+    data.frame(name = x,
+               class = as.character(current_class),
                size = as.numeric(current_size[[1]]),
                unit = current_size[[2]])
   })
 
-  memory_usage <- cbind(name = present_objects, do.call(rbind, memory_usage))
+  # rowbind to one df and add names
+  memory_usage <- do.call(rbind, memory_usage)
 
+  # sort data
   if (sort == "alphabetical") {
     memory_usage <- memory_usage[order(memory_usage$name, decreasing = decreasing), ]
   }
@@ -60,6 +70,11 @@ show_memory_use <- function(sort = "size", units = "Kb", decreasing = FALSE) {
     if(sort != "size") {warning("sort argument unkown - using size",
                                 call. = FALSE)
     }
+  }
+
+  # only print top n rows
+  if(!is.null(n)) {
+    memory_usage <- memory_usage[1:n, ]
   }
 
   return(memory_usage)
