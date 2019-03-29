7c44c14248c9fe0bf7cf50fe92f85385a2c66c9c
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-03-29 10:22:45 +0100

    Update test submit_to_cluster

--- tests/testthat/test-submit_to_cluster.R
+++ tests/testthat/test-submit_to_cluster.R
@@ -8,12 +8,3 @@ test_that("submit_to_cluster works ", {
 
   expect_identical(object = result, expected = list(2,4,6))
 })
-
-
-test_that("submit_to_cluster prints time ", {
-
-  fx = function(x) x * 2
-
-  expect_message(submit_to_cluster(fx, x = 1:3, n_jobs = 1), regexp = paste0("Submitting to HPC at ", Sys.time()))
-  expect_message(submit_to_cluster(fx, x = 1:3, n_jobs = 1), regexp = paste0("Finished at ", Sys.time()))
-})
